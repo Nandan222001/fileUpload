daa4bdedce4b6e3e1656f3923e83040eced2ebbc
Nandan222001 <nandankumar222001@gmail.com>
2025-06-26 18:34:37 +0530

    Integrated NODE MAILER Function

--- models/Files.js
+++ models/Files.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose');
+const nodemailer = require('nodemailer');
+require('dotenv').config();
 
 const fileSchema = new mongoose.Schema({
     name : {
@@ -15,5 +17,28 @@ const fileSchema = new mongoose.Schema({
         type : String
     }
 });
+
+fileSchema.post("save",async function(doc) {
+    console.log("Doc :- ",doc);
+
+    try {
+        const transport = nodemailer.createTransport({
+            host : process.env.HOST_NAME,
+            auth : {
+                user : process.env.USER_MAIL,
+                pass : process.env.USER_PASS,
+            }
+        })
+    
+        const info = transport.sendMail({
+            from : "NkTECH",
+            to: doc.email,
+            subject : "Hello From NKTECH",
+            html : "<h1>Hii Bro !!! </h1><p>THis is from Server of Node.js</p>"
+        })
+    } catch (error) {
+        console.log("Error While Sending Mail :- ",error.message);
+    }
+})
 
 module.exports = mongoose.model('File',fileSchema);
